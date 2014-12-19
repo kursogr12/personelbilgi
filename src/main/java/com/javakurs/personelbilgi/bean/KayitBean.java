9951135767a961eef1d7291e190982b45e9459a0
BEM <BEM@BEM-PC>
2014-12-19 12:24:35 +0200

    theme!!

---
+++ src/main/java/com/javakurs/personelbilgi/bean/KayitBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javakurs.personelbilgi.bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author BEM
+ */
+@Named(value = "kayitBean")
+@Dependent
+public class KayitBean {
+
+    /**
+     * Creates a new instance of KayitBean
+     */
+    public KayitBean() {
+    }
+    
+}
